abcd9d2cf63cbbdada9b9273c4672ceeef933804
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-02-25 16:33:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+# 10-masala
+def matn():
+    print("Xush kelibsiz!")
+
+matn()
+
+# 11-masala
+def salom(ism):
+    print(f"Salom, {ism}!")
+
+res = "Guli"
+salom(res)
+
+# 12-masala
+def raqam(raqam):
+    print(raqam)
+
+res = 100
+raqam(res)
+
+# 13-masala
+def raqam(a, b):
+    print(a + b)
+
+res = 5 + 3
+raqam(5, 3)
+
+# 14-masala
+def yosh(ism, yosh):
+    print(f"{ism} {yosh} yoshda.")
+
+res1 = "Guli"
+res2 =  14
+yosh(res1, res2)
+
+# 15-masala
+def kvadrat(son):
+    print(son ** 2)
+
+res = 20
+kvadrat(res)
+
+# 16-masala
+def matn(matn, soni):
+    print(matn * soni)
+
+res1 = "hello"
+res2 = 12
+matn(res1, res2)
